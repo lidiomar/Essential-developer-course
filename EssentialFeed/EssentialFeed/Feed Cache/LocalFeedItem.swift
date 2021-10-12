797841602eaf920d9e2e1763c382bfaf0a1ec7d2
Lidiomar Fernando dos Santos Machado <lidiomar.machado@NMAC-2035.local>
2021-10-12 13:07:33 -0300

    Move LocalFeedItem to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,22 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Lidiomar Fernando dos Santos Machado on 12/10/21.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
